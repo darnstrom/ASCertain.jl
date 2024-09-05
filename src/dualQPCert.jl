83f49792ae6e1bbdf14ce5c236afbc2d27fe5fbc
Daniel Arnström <daniel.arnstrom@gmail.com>
2024-09-05 13:27:04 +0200

    Correctly normalize A and b in P_theta

--- src/dualQPCert.jl
+++ src/dualQPCert.jl
@@ -394,8 +394,19 @@ function normalize(prob::Union{DualCertProblem,DualLPCertProblem},P_theta,mpQP)
         mpQP.f_theta[:,i]*=norm_factors[i];
         mpQP.W[:,i]*=norm_factors[i];
     end
-    P_theta =(A=P_theta.A,
-              b = P_theta.b,
+    if(isempty(P_theta.A))
+        A = []
+        b = b
+    else
+        b = P_theta.b - P_theta.A'*center;
+        A = P_theta.A
+        for i = 1:nth
+            A[i,:] .*= norm_factors[i]
+        end
+    end
+
+    P_theta =(A=A,
+              b = b,
               lb = -ones(nth),
               ub = ones(nth));
     return prob, P_theta, mpQP
